b6e1a70bddbfccafc461beab6468349fb3a568a4
ibcomputing <ibcomputing@users.noreply.github.com>
2014-08-18 12:35:17 +0100

    Update cellularsignal.js

--- www/cellularsignal.js
+++ www/cellularsignal.js
@@ -1,44 +1,55 @@
-var Cellularsignal = function() {};
+var cellularsignal = function() {};
 				
-	Cellularsignal.prototype.enable = function(params, success, fail) {
-		return cordova.exec( function(args) {
-			success(args);
+	cellularsignal.prototype.enable = function(params, success, fail) {
+	    alert('plugin enable called');
+	    return cordova.exec(function (args) {
+	        alert('plugin enable success');
+	        //success(args);
 		}, function(args) {
-			fail(args);
-		}, 'Cellularsignal', 'enable', [params]);
+		    alert('plugin enable failure');
+		    //fail(args);
+		    console.log(args);
+		}, 'cellularsignal', 'enable', [params]);
 	};
 	
-	Cellularsignal.prototype.disable = function(params, success, fail) {
+	cellularsignal.prototype.disable = function(params, success, fail) {
 		return cordova.exec( function(args) {
 			success(args);
 		}, function(args) {
 			fail(args);
-		}, 'Cellularsignal', 'disable', [params]);
+		}, 'cellularsignal', 'disable', [params]);
 	};	   
 	
 var Cellsignal = function() {};
 	
-	Cellsignal.prototype.enable = function(listener, func) {
-		window.plugins.cellularsignal.enable({success:listener}, 
+	Cellsignal.enable = function(listener, func) {
+	    console.log(window.plugins.cellularsignal);
+	    window.plugins.cellularsignal.enable({ success: listener },
 			function(returnVal) {if(typeof func == "function"){func(returnVal.returnVal);}}, // Success function
-			function(error) {alert('Cellular Signal Enable Failed ' + error)}); // Failure function
+			function (error) {
+			    //alert('Cellular Signal Enable Failed ' + error)
+			}); // Failure function
 	};
 		
-	Cellsignal.prototype.disable = function(func) {
+	Cellsignal.disable = function(func) {
 		window.plugins.cellularsignal.disable({}, 
 			function(returnVal) {if(typeof func == "function"){func(returnVal.returnVal);}}, // Success function
-			function(error) {alert('Cellular Signal Disable Failed ' + error)}); // Failure function
+			function (error) {
+			    //alert('Cellular Signal Disable Failed ' + error)
+			}); // Failure function
 	};
+
+	if (!window.plugins) {
+	    window.plugins = {};
+	}
+	if (!window.plugins.cellularsignal) {
+	    window.plugins.cellularsignal = new cellularsignal();
+	}
+
+	if (module.exports) {
+	    module.exports = cellularsignal;
+	    module.exports = Cellsignal;
+	}
+	
 	
 
-if (!window.plugins) {
-    window.plugins = {};
-}
-if (!window.plugins.cellularsignal) {
-    window.plugins.cellularsignal = new Cellularsignal();
-}
-
-if (module.exports) {
-    module.exports = Cellularsignal;
-    module.exports = Cellsignal;
-}
